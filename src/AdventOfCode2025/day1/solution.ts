1b3e344b02cbe0d48dca8baae7aae9df31951593
james-lane <4426937+james-lane@users.noreply.github.com>
2025-12-01 18:11:59 +0000

    Day 1 part 1 complete

--- src/AdventOfCode2025/day1/solution.ts
+++ src/AdventOfCode2025/day1/solution.ts
@@ -3,31 +3,31 @@ export const part1 = (input: string) => {
 
   let zeroCount = 0
   let pointer = 50
-  const min = 0
-  const max = 100
 
   for (let rowIndex = 0; rowIndex < inputRows.length; rowIndex++) {
     let row = inputRows[rowIndex]
-    console.log({ pointer, zeroCount }, row)
 
     if (row.startsWith('L')) {
       for (let index = 0; index < parseInt(row.substring(1)); index++) {
         pointer--
-        if (pointer === max || pointer === max) pointer = max
-        console.log('pointer after minus', pointer)
+
+        if (pointer === 100 || pointer === -100) {
+          pointer = 0
+        }
       }
     }
 
     if (row.startsWith('R')) {
       for (let index = 0; index < parseInt(row.substring(1)); index++) {
         pointer++
-        if (pointer === max || pointer === max) pointer = min
-        console.log('pointer after plus', pointer)
+
+        if (pointer === 100 || pointer === -100) {
+          pointer = 0
+        }
       }
     }
 
-    if (pointer === min) zeroCount++
-    console.log({ pointer, zeroCount })
+    if (pointer === 0) zeroCount++
   }
 
   return zeroCount
