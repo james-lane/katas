3f57845d8790de1219df5e8541ab61141ae49422
james-lane <4426937+james-lane@users.noreply.github.com>
2025-12-02 08:59:17 +0000

    Day 2 part 1 complete

---
+++ src/AdventOfCode2025/day2/solution.ts
@@ -0,0 +1,26 @@
+export const part1 = (input: string) => {
+  const ranges: string[] = input.split(',')
+
+  const invalidIds: number[] = []
+
+  ranges.forEach((range) => {
+    const firstInRange = parseInt(range.split('-')[0])
+    const lastInRange = parseInt(range.split('-')[1])
+
+    for (let currentNumber = firstInRange; currentNumber <= lastInRange; currentNumber++) {
+      const numberAsString = currentNumber.toString()
+      const numberLength = numberAsString.length
+
+      const firstHalf = numberAsString.substring(0, numberLength / 2)
+      const lastHalf = numberAsString.substring(numberLength / 2)
+
+      if (firstHalf === lastHalf) {
+        invalidIds.push(currentNumber)
+      }
+    }
+  })
+
+  return invalidIds.reduce((a, b) => a + b)
+}
+
+export const part2 = (input: string) => {}
